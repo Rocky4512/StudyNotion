06b2fed85a8dcbb0b9b2cc3a12d9a99edb567d54
Rohit <rohitagarwal04796@gmail.com>
2025-07-29 13:02:46 +0530

    changes in api

--- frontend/src/services/apis.js
+++ frontend/src/services/apis.js
@@ -1,4 +1,4 @@
-const BASE_URL = import.meta.env.VITE_APP_BASE_URL ?? "https://studynotion-backend-ur6y.onrender.com";
+const BASE_URL = import.meta.env.VITE_APP_BASE_URL;
 
 // AUTH ENDPOINTS
 export const endpoints = {
