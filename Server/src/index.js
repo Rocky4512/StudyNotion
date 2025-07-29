4abab258c22d0abc7ba6075a0e3813afbd193370
Rohit <rohitagarwal04796@gmail.com>
2025-07-29 14:41:56 +0530

    origin

--- Server/src/index.js
+++ Server/src/index.js
@@ -23,7 +23,7 @@
     app.use(cookieParser());
     app.use(
     cors({
-        origin: [process.env.CLIENT_URL],
+        origin: "https://study-notion-5ula.vercel.app",
         credentials: true,
     }),
     );
